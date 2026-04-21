8166663b5cfd2598dcd663d4274a63890abfd252
Vishal Pawar <130729939+Vishalp-awar@users.noreply.github.com>
2026-04-22 02:22:27 +0530

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,11 +1,13 @@
+// db.ts
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("❌ MONGODB_URI is not defined in environment variables");
+  throw new Error("❌ Please define MONGODB_URI in environment variables");
 }
 
+// global caching (important for Next.js)
 let cached: any = global.mongoose || { conn: null, promise: null };
 
 export async function connectDB() {
